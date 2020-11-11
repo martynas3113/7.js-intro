57bb9355b07746560d9f67089a019af0d4bc463d
martynas3113 <martynas3113@users.noreply.github.com>
2020-11-11 15:47:56 +0200

    intro

---
+++ js/main.js
@@ -0,0 +1,48 @@
+// Kintamieji elementai
+
+
+
+
+
+// Analogas
+// Kintamuju tipai
+    // skaiciai (number) - 20 , 50 ,10 
+    // tesktai (string) = 'ahuy ' , 'Yes' 
+    // logines (boolean) - true , false 
+    // sarasai (array) - []
+    // objektai (object) - {}
+
+const currentTime = new Date();
+const year = currentTime.getFullYear();
+const heroname = 'Chuck';
+const heroLastname = 'Norris';
+const birth = 1940;
+
+const age = year-birth;
+const sentence = heroname + ' '  +   heroLastname + ' ('+ age + ')' + ' years old';
+
+console.log(sentence);
+
+console.clear()
+
+let wallet = 8;
+const sausis = 100;
+const vasaris = 200;
+const kovas = 70;
+
+wallet = wallet + sausis;
+console.log(wallet)
+wallet = wallet + vasaris;
+console.log(wallet)
+wallet = wallet + kovas;
+console.log(wallet)
+
+
+
+
+
+
+
+
+
+
